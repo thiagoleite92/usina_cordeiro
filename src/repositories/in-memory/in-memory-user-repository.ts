62a04dfc592aada82be5d81f9df10ef074fe4ddd
Thiago Leite <leite.tjs@gmail.com>
2024-03-13 14:33:45 -0300

    test: find by user id test

--- src/repositories/in-memory/in-memory-user-repository.ts
+++ src/repositories/in-memory/in-memory-user-repository.ts
@@ -13,4 +13,14 @@ export class InMemoryUserRepository implements UsersRepositoryInterface {
 
     return user;
   }
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
 }
